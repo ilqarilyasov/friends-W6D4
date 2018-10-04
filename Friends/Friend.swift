f2f906f98bd5538485600eb67903c40b07aa2e48
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2018-10-04 15:23:24 -0500

    Images and friends added

---
+++ Friends/Friend.swift
@@ -0,0 +1,23 @@
+//
+//  Friend.swift
+//  Friends
+//
+//  Created by Ilgar Ilyasov on 10/4/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class Friend {
+    
+    let name: String
+    let image: UIImage
+    let bio: String
+    
+    init(name: String, image: UIImage, bio: String) {
+        self.name = name
+        self.image = image
+        self.bio = bio
+    }
+    
+}
---
+++ Friends/FriendController.swift
@@ -0,0 +1,23 @@
+//
+//  FriendController.swift
+//  Friends
+//
+//  Created by Ilgar Ilyasov on 10/4/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class FriendController {
+    
+    let friends = [
+        Friend(name: "Mark", image: #imageLiteral(resourceName: "Mark"), bio: "iOS Developer"),
+        Friend(name: "Marion", image: #imageLiteral(resourceName: "Marion"), bio: "Android Developer"),
+        Friend(name: "Jessica", image: #imageLiteral(resourceName: "Jessica"), bio: "Full Stack Developer"),
+        Friend(name: "Rolf", image: #imageLiteral(resourceName: "Rolf"), bio: "UI/UX Designer"),
+        Friend(name: "Sam", image: #imageLiteral(resourceName: "Sam"), bio: "Mac Developer"),
+        Friend(name: "Jennifer", image: #imageLiteral(resourceName: "Jennifer"), bio: "HR manager"),
+        Friend(name: "Christine", image: #imageLiteral(resourceName: "Christine"), bio: "Operations manager")
+    ]
+    
+}
