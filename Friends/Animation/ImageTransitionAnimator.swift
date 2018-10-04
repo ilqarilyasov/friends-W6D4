cb734fb97df15b070cde2ad9ba22f39376bbdce9
Ilqar Ilyasov <ilqar_ilyasov@yahoo.com>
2018-10-04 16:05:29 -0500

    ImageTransitionAnimator file created

---
+++ Friends/Animation/ImageTransitionAnimator.swift
@@ -0,0 +1,20 @@
+//
+//  ImageTransitionAnimator.swift
+//  Friends
+//
+//  Created by Ilgar Ilyasov on 10/4/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ImageTransitionAnimator: NSObject, UIViewControllerAnimatedTransitioning {
+    
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 0.5
+    }
+    
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        <#code#>
+    }
+}//end of class
